0da3bd27f9d6425f895d261dc0b4a5d244bf6601
Zabro <reinzabrocki@shaw.ca>
2024-03-01 17:45:23 -0700

    Commenting and cleaning functions

--- main.py
+++ main.py
@@ -2,27 +2,7 @@
 
 import csv
 import os.path
-
-
-#This function will handle the "front screen"       
-def Game_Intro():
-    print("Welcome to the ODWM gesser game! I, the computer will guess what physicist you are thinking of!")
-    # print out some rules and maybe some code so I can play
-
-
-
-def Game_Settings():
-    print("here are the game settings")
-    print("Do you want only ODWM?")
-    odwm = input()
-    print("What physics unit do you want? Type 1 for all, type 2 for F&F, type 3 for EMR, type 4 for Atomic")
-    unit = input()
-    print("Do you only want to play with physicists whose expariments are on the diploma?")
-    diploma = input()
-    
-    return odwm, unit, diploma
    
-
 class Scientist:
 
     def __init__(self, name, num_name, physics_unit, respose_list, expariment, odwm, special_questions):
@@ -58,9 +38,27 @@ class Scientist:
         pass
 
 
-    def Game_code(scientists):
-        od, unit, diploma_exp = Game_Intro()
+#This function will handle the "front screen"       
+def Game_Intro():
+    print("Welcome to the ODWM gesser game! I, the computer will guess what physicist you are thinking of!")
+    # print out some rules and maybe some code so I can play
 
+
+def Game_Settings():
+    print("here are the game settings")
+    print("Do you want only ODWM?")
+    odwm = input()
+    print("What physics unit do you want? Type 1 for all, type 2 for F&F, type 3 for EMR, type 4 for Atomic")
+    unit = input()
+    print("Do you only want to play with physicists whose expariments are on the diploma?")
+    diploma = input()
+    
+    return odwm, unit, diploma
+
+def Name_Print(name_list):
+    for people in name_list:
+        print(people.name)
+                
 def Scientist_Sort(sci_list):
 
     man, unit, dip = Game_Settings()
@@ -98,15 +96,11 @@ def Scientist_Sort(sci_list):
         sci_list = sci_list2
 
     return sci_list
-                
 
 
-#This is the function that creates, opens, and handles the main list of questions and scientist's reponsies to them and esnures all the elements in the rows are filled out
-#This function will also asign or read the row numbers assosiated with each scientist
-#How do I load the questions and the socres?
 
-#The goal of this function is to build the big list of questions to randomly choose from 
-game_file_name = "Questions_and_Answers.csv"
+ 
+#The goal of this function is to build the big list of questions that will be the base for the game
 def Loading_Game_Data(file_name, selected_sci_list):
     #this will look for the main responses list file
     #This code will look to see if the game file exists 
@@ -160,38 +154,20 @@ def Loading_Question_Responses(file_name, selected_sci_list):
     del response_list[-1]
     print("response list", response_list)
 
-    for responder in response_list:
-        temp_small_list = responder
-        for scientist in selected_sci_list:
-            if scientist.name == temp_small_list[0]:
-                scientist.response_asign(temp_small_list)
-
-   
-        
-
-
-
-
-
-
-
-
-
     # for all scientists selected, use their number name index to load reponses to the questions and make it a new attrubute to them
+    for small_list in response_list:
+
+        #then looks at all the scientists in the game
+        for scientist in selected_sci_list:
+
+            #checks to see if the name of the current scientist object is equal to the name assosaited with the response list
+            #if so then that is the response list for the object and a new attrubute is made 
+            if scientist.name == small_list[0]:
+                scientist.response_asign(small_list)
 
 
 
-
-
-def Name_Print(name_list):
-    for people in name_list:
-        print(people.name)
-
-
-
-
-
-
+game_file_name = "Questions_and_Answers.csv"
 
 sci_1_questions = ["Did my experimental apparatus originally use water droplets, but in 1910 I changed the substance because the droplets evaporated too fast to make measurements?", "Was a key piece of work I built off of the charge to mass ratio of an electron that was discovered by Thomson?", "In 1916, did I also devise another experiment to measure another fundamental constant? Did I also use a previous constant I measured in my calculations?"] 
 
@@ -202,22 +178,33 @@ sci_3 = Scientist("Coulomb", 3,2, None, 1, 1, None)
 sci_4 = Scientist("Henry",4,2,None,0,1,None)
 sci_5 = Scientist("Einstein",5,3,None,0,1,None)
 
+#The list of all the scientist objects
 scientist_list = [sci_1, sci_2, sci_25, sci_3, sci_4, sci_5]
-selected_sci_list = []
 
-#runs the filter selection code so and lets user know about how big the game will be
-#if less than or equal to one scientist is in the game that wouldnt be that fun so the user will choose new filter parameters
+#List of the scientist objects that made it through the filter
+selected_sci_list = []
+questions_dump = []
+
+
+#The game code:
+#////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
 Game_Intro()
 
 while len(selected_sci_list) <= 1:
+    #runs the filter selection code so and lets user know about how big the game will be
+    #if less than or equal to one scientist is in the game that wouldnt be that fun so the user will choose new filter parameters
+    
     selected_sci_list = Scientist_Sort(scientist_list)
     print(f"The parameters you selected resulted in {len(selected_sci_list)} scientists present in the game")
+
     if len(selected_sci_list) <= 1:
         print("Pick new filtes because that number of scientists wouldnt be fun to play with")
 
-Name_Print(selected_sci_list)
+#Name_Print(selected_sci_list)
 
-Loading_Game_Data(game_file_name, selected_sci_list)
+questions_dump = Loading_Game_Data(game_file_name, selected_sci_list)
 
 Loading_Question_Responses(game_file_name, selected_sci_list)
 
